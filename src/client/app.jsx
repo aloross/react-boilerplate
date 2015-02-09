0830f7388c28b4d21785884265235fdc52092981
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2015-02-09 13:23:44 +0100

    Base project structure

---
+++ src/client/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Test from './home'
+
+React.render(
+	<Home />,
+	document.getElementById('main')
+)
---
+++ src/client/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default React.createClass({
+	displayName: 'Home',
+	render: function () {
+		return (
+			<div>Hello world!</div>
+		)
+	}
+})
